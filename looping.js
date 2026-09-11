ae4282b38e0c667bd56b57e08738a37149605bb1
Muhammad Iqbal <longpaje@gmail.com>
2026-09-11 17:34:56 +0700

    while

---
+++ looping.js
@@ -0,0 +1,9 @@
+let pengecekan = 20;
+
+for (let i = 0; i <= 20; i++) {
+    if (i % 2 === 0) {
+        console.log(i, "Adalah genap");
+    } else {
+        console.log(i, "Adalah ganjil");
+    }   
+}
---
+++ while.js
@@ -0,0 +1,10 @@
+let i = 1
+
+while (i <= 19) {
+    i++;
+    if (i % 2 === 0) {
+        console.log(i, "bilang ganjil");
+    } else {
+        console.log(i, "bilangan genap");
+    }
+}
